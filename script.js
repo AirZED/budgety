193380fa16fe651388d55327e2482c9c5afa532d
Mfoniso <ukpabiomfoniso@gmail.com>
2022-06-09 02:49:17 +0100

    Add to list functionality added

--- script.js
+++ script.js
@@ -1,94 +1,138 @@
-`use strict`;
-let totalAmount = 0;
-let IndividualArray;
-let List;
+//BUDGET CONTROLLER
+const budgetController = (function () {
+  let Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
+  let Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
-const Money = function (total, list) {
-  this.list = list;
-  this.total = total;
-};
+  let data = {
+    allitems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      totalExpenses: 0,
+      totalIncomes: 0,
+    },
+  };
 
-const income = new Money(5000, [
-  ["Website Project", 500],
-  ["Banga Soup", 600],
-]);
+  return {
+    additem: function (type, des, val) {
+      let newItem;
 
-const expense = new Money(2000, [
-  ["Buy Shoes", 200],
-  ["Banga Soup", 600],
-]);
+      //Create New ID
+      data.allitems[type].length <= 0
+        ? (ID = 0)
+        : (ID = data.allitems[type][data.allitems[type].length - 1].id + 1);
 
-const users = [income, expense];
+      //Create new Item basedon 'inc' and 'exp'
+      if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      } else if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      }
+      //push newitem into our data structure
+      data.allitems[type].push(newItem);
+      return newItem;
+    },
+    testing: function () {
+      console.log(data);
+    },
+  };
+})();
 
+//UI CONTROLLER
+const UIController = (function () {
+  let DomStrings = {
+    inputType: "#selector",
+    descriptionType: ".add__description",
+    valueType: ".add__value",
+    submitBtn: "button",
+  };
 
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DomStrings.inputType).value, //Will be either income or expense
+        description: document.querySelector(DomStrings.descriptionType).value,
+        value: document.querySelector(DomStrings.valueType).value,
+      };
+    },
+    addListItem: function (obj, type)
+    {
+      let html;
+      //Create html string with placeholder text
 
-function submitNewList(e) {
-  e.preventDefault();
+      if (type === 'inc') {
+        html = `<li id= "${obj.id}">
+          <div>${obj.description}</div> 
+          <div class="income-amount">$${obj.value}<span>25%</span><span>x</span></div>
+      </li>`;
+      } else if (type === 'exp') {
 
-  addDescription = document.querySelector(`.add-description`).value;
-  addValue = Number(document.querySelector(`.value`).value);
+        html = `<li id= "${obj.id}">
+                <div>${obj.description}</div>
+                <div class="expense-amount">$${obj.value}<span>50%</span></div>
+              </li>`;
+      }
 
-  if (addDescription === ``) {
-    console.log("Input Description");
-  } else if (addValue === ``) {
-    console.log(`Input Value`);
-  } else {
-    IndividualArray = [addDescription, addValue];
-    List.push(IndividualArray);
-    expense.displayList('expense');
-    expense.calcTotalEI('expense');
-  }
+      //Replace the placeholder text with some real data
+      document.querySelector(`.${type}-adons`).insertAdjacentHTML(`afterbegin`, html);
+      
 
-  document.querySelector(`.add-description`).value = "";
-  document.querySelector(`.value`).value = "";
-}
-Money.prototype.appendArray = function () {
-  let addDescription;
-  let addValue;
-  List = this.list;
+      //Insert the html into the DOM
+    },
+    getDOMString: function () {
+      return DomStrings;
+    },
+  };
+})();
 
-  document.querySelector("button").addEventListener("click", submitNewList);
-};
+//GLOBAL APP CONTROLLER
+//In order for this branch to access the other functions, they would be passed as arguments to this function below
+const controller = (function (budgetCtrl, UICtrl) {
+  let setupEventListeners = function () {
+    let DOM = UICtrl.getDOMString();
+    document
+      .querySelector(DOM.submitBtn)
+      .addEventListener("click", ctrlAddItem);
+    document.addEventListener("keypress", function clickedBtn(event) {
+      //SINCE NOT ALL BROWSER USES KEYCODES; SOME USES WHICH
+      if (event.keyCode === 13 || event.which === 13 || event.key === "Enter") {
+        ctrlAddItem();
+      }
+    });
+  };
 
+  let ctrlAddItem = function () {
+    let input, newItem;
+    //Get the filled input data
 
+    input = UICtrl.getInput();
 
+    //Add the item to the budget controller
+    newItem = budgetCtrl.additem(input.type, input.description, +input.value);
+    //add the item to the UI
+    UICtrl.addListItem(newItem, input.type);
 
-expense.appendArray();
+    //Calculate the budget
 
+    //Display the budget on the UI
+  };
 
-Money.prototype.displayList = function (name) {
-  let mapExpense = List
-    .map(
-      (element) =>
-        ` <li>
-          <div>${element[0]}</div>
-          <div class="${name}-amount">$${element[1]}</div>
-        </li>`
-    )
-    .join(``);
+  return {
+    initialization: function () {
+      console.log("App has started");
+      setupEventListeners();
+    },
+  };
+})(budgetController, UIController);
 
-  document.querySelector(`.${name}-adons`).innerHTML = mapExpense;
-};
-
-expense.displayList("expense");
-income.displayList(`income`);
-
-Money.prototype.calcTotalEI = function (name) {
-  let dividedTotal = 0;
-  for (let index = 0; index < this.list.length; index++) {
-    dividedTotal = dividedTotal + this.list[index][1];
-  }
-
-  console.log(dividedTotal);
-  document.querySelector(`.${name}-amount`).textContent = `+${dividedTotal}`;
-};
-
-expense.calcTotalEI("expense");
-income.calcTotalEI("income");
-
-
-
-
-document.querySelector(".add-expense").addEventListener(`click`, (console.log('Clicked')));
-
+controller.initialization();
